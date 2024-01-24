8d62f9b66a8eaddd3be7a4dc67bc0ef6f00ca91a
io <1134921599@qq.com>
2024-01-25 04:30:34 +0800

    learn animation

--- FunnyFox/Assets/PlayerMovement.cs
+++ FunnyFox/Assets/PlayerMovement.cs
@@ -6,27 +6,57 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D Controller;
+    public Animator Animator;
 
     public float RunSpeed = 40f;
 
-    float horizontalMove = 0f;
-    bool jump = false;
+    private float _horizontalMove = 0f;
+    private bool _jump = false;
+    private bool _crouch = false;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+    private static readonly int IsCrouching = Animator.StringToHash("IsCrouching");
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
 
-        if (Input.GetKeyDown("space"))
+        Animator.SetFloat(Speed, Math.Abs(_horizontalMove));
+
+        // when the key is pressed
+        if (Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            _jump = true;
+            Animator.SetBool(IsJumping, true);
         }
+
+        // when the key is pressed
+        if (Input.GetButtonDown("Crouch"))
+        {
+            _crouch = true;
+        }
+        // when the key is loosen
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            _crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        Animator.SetBool(IsJumping, false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        Animator.SetBool(IsCrouching, isCrouching);
     }
 
     private void FixedUpdate()
     {
         // Move our character
-        Controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        Controller.Move(_horizontalMove * Time.fixedDeltaTime, _crouch, _jump);
+        _jump = false;
     }
 }
